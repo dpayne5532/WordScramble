5546c6a7e6acd493367ce0bf4e857bf4ba64b645
Dan Payne <dpayne5532@gmail.com>
2022-01-16 07:47:13 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dan Payne on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
